f7f5660e23d746f01ce9d0dd948b565a0f69e839
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-06-27 01:03:11 +0200

    Bug

--- src/main/scala/org/dt/Hello.scala
+++ src/main/scala/org/dt/Hello.scala
@@ -1,7 +1,15 @@
 package org.dt
 
-object Hello extends App {
-  println("Hello")
+object Hello {
+  def printHello(name: String): String = name match {
+    case name if name.isBlank => "Hello World!"
+    case _ => "Hello " concat name
+  }
+
+  def sumPairs(x: Int, y: Int): Int = (x, y) match {
+    case (x, y) if x % 2 == 0 => x + y
+    case _ => x - y
+  }
 }
 
 object Main {
--- src/test/scala/org/dt/HelloSpec.scala
+++ src/test/scala/org/dt/HelloSpec.scala
@@ -6,4 +6,12 @@ class HelloSpec extends AnyFunSuite {
   test("Hello should start with H") {
     assert("Hello".startsWith("H"))
   }
+
+  test("Print hello print world") {
+    assert(Hello.printHello(" ").equals("Hello World!"))
+  }
+
+  test("Print hello say hello!") {
+    assert(Hello.printHello("Daniele").equals("Hello Daniele"))
+  }
 }
