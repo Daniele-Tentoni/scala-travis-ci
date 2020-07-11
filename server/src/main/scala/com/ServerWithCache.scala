fed32ff001eac374c1a212ecc2e3027d5adac0d0
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-07-11 11:37:27 +0200

    Implement some basic property test on server

--- server/src/main/scala/com/ServerWithCache.scala
+++ server/src/main/scala/com/ServerWithCache.scala
@@ -1,5 +1,30 @@
 package com
 
-class ServerWithCache {
+case class Request(body: String)
+case class Response(content: String)
 
+class Server {
+  def processRequest(req: Request): Response = Response(req.body + "'s response")
+  def serve(req: Request): Response = processRequest(req)
+}
+
+trait Cache[K, T] {
+  def cached(key: K): Boolean
+  def get(key: K): T
+  def put(key: K, value: T): Unit
+}
+
+class ServerWithCache(
+                     private val cache: Cache[Request, Response],
+                     private val cacheable: Request => Boolean)
+extends Server {
+  override def serve(req: Request): Response = if (!cacheable(req)) super.serve(req) else {
+    if (cache.cached(req)) {
+      cache.get(req)
+    } else {
+      val res = processRequest(req)
+      cache.put(req, res)
+      res
+    }
+  }
 }
--- server/src/test/scala/com/ServerTestWithMock.scala
+++ server/src/test/scala/com/ServerTestWithMock.scala
@@ -1,5 +1,37 @@
 package com
 
-class ServerTestWithMock {
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.Matchers
+import org.scalatest.funsuite.AnyFunSuite
 
+/**
+ * Test server with Mock support by ScalaMock.
+ */
+class ServerTestWithMock extends AnyFunSuite with MockFactory with Matchers {
+  test("Test server with cache") {
+    // Mocks/stubs for dependencies.
+    val cacheMock = mock[Cache[Request, Response]]
+    val cachebleStub = stubFunction[Request, Boolean]
+    cachebleStub.when(*).returns(true)
+
+    // Wire Sut with stubbed/mocked dependecies.
+    val server= new ServerWithCache(cacheMock, cachebleStub)
+
+    // Arrange.
+    val request = Request("Some request")
+    val expectedResponse = Response(request.body + "'s response")
+
+    // Mock expectations.
+    inSequence {
+      (cacheMock.cached _).expects(request).returning(false)
+      (cacheMock.put _).expects(request, *)
+      (cacheMock.cached _).expects(request).returning(true)
+      (cacheMock get _).expects(request).returning(expectedResponse)
+    }
+
+    // Act + Assert
+    server.serve(request) shouldEqual expectedResponse
+    server.serve(request) shouldEqual expectedResponse
+    cachebleStub verify request twice()
+  }
 }
